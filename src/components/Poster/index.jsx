6090c8082a662bca256c81d9fa065f084f2dacdd
rajprakashmaurya <raj232033@gmail.com>
2023-03-18 08:54:23 +0530

    poster setup

--- src/components/Poster/index.jsx
+++ src/components/Poster/index.jsx
@@ -5,46 +5,50 @@ import "./poster.css";
 
 function Poster() {
   return (
-    <Box className="poster">
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        color="white"
-        gap={25}
-      >
-        <Box my={20} textAlign="center">
-          <Typography fontSize={50} fontWeight={600}>
-            232
-          </Typography>
-          <Typography fontWeight={600} color="gray">
-            Clients
-          </Typography>
-        </Box>
-        <Box textAlign="center">
-          <Typography fontSize={50} fontWeight={600}>
-            232
-          </Typography>
-          <Typography fontWeight={600} color="gray">
-            Projects
-          </Typography>
-        </Box>
-        <Box textAlign="center">
-          <Typography fontSize={50} fontWeight={600}>
-            1453
-          </Typography>
-          <Typography fontWeight={600} color="gray">
-            Hours Of Support
-          </Typography>
-        </Box>
-        <Box textAlign="center">
-          <Typography fontSize={50} fontWeight={600}>
-            32
-          </Typography>
-          <Typography fontWeight={600} color="gray">
-            Workers
-          </Typography>
-        </Box>
+    <Box
+      sm={12}
+      xl={12}
+      className="poster"
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      color="white"
+      flexWrap="wrap"
+      textAlign="center"
+      px={8}
+      Overflow="hidden"
+    >
+      <Box p={2}>
+        <Typography fontSize={50} fontWeight={600}>
+          232
+        </Typography>
+        <Typography fontWeight={600} color="gray">
+          Clients
+        </Typography>
+      </Box>
+      <Box p={2}>
+        <Typography fontSize={50} fontWeight={600}>
+          232
+        </Typography>
+        <Typography fontWeight={600} color="gray">
+          Projects
+        </Typography>
+      </Box>
+      <Box p={2}>
+        <Typography fontSize={50} fontWeight={600}>
+          1453
+        </Typography>
+        <Typography fontWeight={600} color="gray">
+          Hours Of Support
+        </Typography>
+      </Box>
+      <Box p={2}>
+        <Typography fontSize={50} fontWeight={600}>
+          32
+        </Typography>
+        <Typography fontWeight={600} color="gray">
+          Workers
+        </Typography>
       </Box>
     </Box>
   );
